2bc9e753e068807b42db349e30fe9c5e4669ac67
Daniele Caputo <caputo.daniele.n1@gmail.com>
2018-09-19 23:31:50 +0200

    Add SignIn, SignUp functions Add Firebase API for login and user registration

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -9,6 +9,45 @@
  */
 angular.module('eventPlannerApp')
   .controller('LoginCtrl', function () {
-    this.username = '';
-    this.password = '';
+    this.email;
+    this.password;
+    this.user;
+    this.displayName;
+
+    //logIn
+    this.logIn = () => {
+      firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+        .then((response) => {
+          console.log(response);
+          this.user = response.user;
+          this.displayName = response.user.displayName;
+          this.loggedYN();
+        })
+        .catch(function (error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // [START_EXCLUDE]
+          if (errorCode === 'auth/wrong-password') {
+            alert('Wrong password.');
+          } else {
+            alert(errorMessage);
+          }
+          console.log(error);
+        });
+    }
+
+    //log out
+    this.logOut = () => {
+      firebase.auth().signOut();
+      this.loggedYN();
+      this.email = '';
+      this.password = '';
+      this.user = '';
+      this.displayName = '';
+    }
+
+    this.loggedYN = () => {
+      $('#events').toggleClass('disabled');
+    }
   });
--- app/scripts/controllers/registration.js
+++ app/scripts/controllers/registration.js
@@ -10,7 +10,38 @@
 angular.module('eventPlannerApp')
   .controller('RegistrationCtrl', function () {
     this.name = '';
-    this.username = '';
-    this.password = '';
     this.email = '';
+    this.password = '';
+    this.user;
+
+    this.signUp = () => {
+      firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+        .then((response) => {
+          console.log(response.user);
+          this.user = response.user;
+          this.updateProfile(this.user, this.name);
+          alert('user Successfully Registered')
+        })
+        .catch(function (error) {
+          // Handle Errors here.
+          let errorCode = error.code;
+          let errorMessage = error.message;
+          console.log(errorCode);
+          console.log(errorMessage);
+        });
+    }
+
+    //update user Information
+    this.updateProfile = (user, name) => {
+      user.updateProfile({
+        displayName: name,
+      }).then(function () {
+        // Update successful.
+        alert('Update successful');
+      }).catch(function (error) {
+        // An error happened.
+        alert('An error happened.');
+        console.log(error);
+      });
+    }
   });
